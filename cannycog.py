76ebb3314b0de2bf8cb4a312824993cf2df82eff
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-03-12 15:46:45 +0500

    corrected many mistakes returned to old one

--- cannycog.py
+++ cannycog.py
@@ -3,32 +3,21 @@ from urllib.request import Request, urlopen
 import numpy as np
 from discord.ext import commands
 import discord
-from utils.asyncfunc import asyncexe
-from io import BytesIO
 
 class custom(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @asyncexe()
-    def to_image(self, bytes, readFlag):
-        image = np.asarray(bytearray(bytes), dtype="uint8")
+    def url_to_image(self,url, readFlag=cv.IMREAD_COLOR):
+        # download the image, convert it to a NumPy array, and then read
+        # it into OpenCV format
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        resp = urlopen(req).read()
+        image = np.asarray(bytearray(resp), dtype="uint8")
         image = cv.imdecode(image, readFlag)
-        return image
-    
-    async def url_to_image(self, url, readFlag=cv.IMREAD_COLOR):
-        # download the image, convert it to a NumPy array, and read the numpy array
-        async with self.bot.session.get(url) as resp:
-            image_bytes = await resp.read()
-        image = await self.to_image(image_bytes, readFlag)
+
         # return the image
-        return image   
-    @asyncexe()
-    def _canny(self, image):
-        canny = cv.Canny(image, 125, 175)
-        is_success, image_buffer = cv2.imencode(".png", canny)
-        buffer = BytesIO(image_buffer)
-        return discord.File(buffer, "canny.png")
+        return image
 
     @commands.command()
     async def canny(self,ctx, member: discord.Member = None):
@@ -36,9 +25,11 @@ class custom(commands.Cog):
             member = ctx.message.author
         else:
             member = member
-        img = await self.url_to_image(member.avatar_url)    
-        file = await self._canny(img)
-        await ctx.send(file=file) 
+        img = self.url_to_image(member.avatar_url)    
+        canny = cv.Canny(img, 125, 175)
+        cv.imwrite("new_image.jpg", canny)
+        file=discord.File('new_image.jpg')
+        await ctx.send(file=file)
 
 def setup(bot):
     bot.add_cog(custom(bot))
